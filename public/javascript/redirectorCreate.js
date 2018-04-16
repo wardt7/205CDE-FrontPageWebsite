9e67154c9487d8c1d6345492e530866bfa6706d2
Toby Ward <wardt7@uni.coventry.ac.uk>
2018-04-16 08:52:10 -0400

    Generally this will be the final commit unless there is a major issue found. Documentation has now been added (note that the JSDoc scripts are external and therefore don't comply with the linter's standards, so make sure to exclude 'out' folders when linting), and a few tweaks have been made to avoid unneccesary variables, etc.

--- public/javascript/redirectorCreate.js
+++ public/javascript/redirectorCreate.js
@@ -1,6 +1,11 @@
 
 'use strict'
 
+/**
+* Client side code used to redirect clients to the home page if they try to sign up again whilst logged in
+* @module redirectorCreate
+*/
+
 if(sessionStorage.token) {
 	location.replace('/')
 }
--- public/javascript/redirectorSubmit.js
+++ public/javascript/redirectorSubmit.js
@@ -1,13 +1,27 @@
 
 'use strict'
-
+/**
+* Client side code used to redirect clients to the sign up page if they access the submit page without being logged in.
+* The code uses XMLHttpRequest to send an authorization request to the server; if it rejects, then the client is redirected
+* @module redirectorSubmit
+* @requires XMLHttpRequest
+*/
 const NOT_AUTHORIZED = 401
 const token = sessionStorage.getItem('token')
 
+// Check to see if the token exists; if it doesn't, then immediately redirect
 if(!sessionStorage.token) {
 	location.replace('/create')
 }
 
+/**
+* XMLHttpRequest that authorizes the user
+* @function XMLHttpRequest/checkauth
+* @param {string} requestType - GET
+* @param {string} path - /checkauth
+* @param {boolean} async - We set the async property to true for time efficiency
+* @returns {object} status - We receive a status code, and redirect based upon that code
+*/
 const xhr = new XMLHttpRequest()
 xhr.open('GET', '/checkauth', true)
 xhr.setRequestHeader('authorization', `Basic ${token}`)
